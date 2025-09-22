4de049097285ac3afe1101276145f29ee3be98b3
the-gak <gahileshkumar@gmail.com>
2025-09-23 00:03:30 +0530

    Conditional basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+//Conditional base path for GitHub Pages
+const getBasePath = () => {
+	if (process.env.NODE_ENV === "production") {
+		return "/gak-portfolio"; // Replace with your repository name
+	} else if (process.env.NODE_ENV === "staging") {
+		return "/";
+	}
+	return "/";
+}
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	site: "https://www.ahilesh.com",
-  	base: "/gak-portfolio",
+  	base: getBasePath(),
   	trailingSlash: "ignore",
 });
