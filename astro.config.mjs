ddd1b58feaa3ca8b7d8aeca1c1b93d5956522cd8
the-gak <gahileshkumar@gmail.com>
2025-09-22 23:45:14 +0530

    Path changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	site: "https://www.ahilesh.com",
+  	base: "/gak-portfolio",
+  	trailingSlash: "ignore",
 });
