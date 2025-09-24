d6e7a3fffdd62a5eb60f08617323be4e507b6dca
the-gak <gahileshkumar@gmail.com>
2025-09-24 15:50:22 +0530

    Added a resources page

--- src/content/config.js
+++ src/content/config.js
@@ -9,6 +9,17 @@ const postCollection = defineCollection({
 	}),
 });
 
+const resourcesCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		dateFormatted: z.string(),
+		resourceSlug: z.string(),
+	}),
+});
+
 export const collections = {
 	post: postCollection,
+	resources: resourcesCollection,
 };
---
+++ src/content/resources/api.md
@@ -0,0 +1,7 @@
+---
+title: "My Resource"
+description: "Description here"
+dateFormatted: "2025-09-24"
+resourceSlug: "my-resource"
+---
+Content goes here.
